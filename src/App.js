787ca2246e1573ba1038140e26e81aa04d1919b8
Parth Brahmaxatri <parthbrahmaxatri007@gmail.com>
2021-06-17 20:00:04 +0530

    :sparkles: add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./parts/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
     </div>
   );
 }
---
+++ src/parts/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Events & News</h1>
+            <h4>Learn, Compete & Grow</h4>
+        </div>
+    )
+}
+
+export default Header;
